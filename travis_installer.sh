0ddc9edb46a3e1a21efd13efd9a474639359051a
zihaolucky <zihaolucky@gmail.com>
2017-02-27 23:23:15 +0800

    disable xcode6 variants and fixed patch.

--- travis_installer.sh
+++ travis_installer.sh
@@ -31,10 +31,10 @@ bash bazel-bin/tensorflow/tools/pip_package/build_pip_package.sh ../python/dist/
 set -eo pipefail
 
 cd ..
-rm python/README*
-cp -r python/* ../
 # python-binary issue
 git apply ../tools/travis_wheel/python-binary.patch
+rm python/README*
+cp -r python/* ../
 pip install python/dist/*.whl
 
 # clean up
